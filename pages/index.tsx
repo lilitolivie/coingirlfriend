a1725865a2bee15e28ea3c1ac6f68b5b3fd70335
lilitolivie <zipizigizumi@gmail.com>
2025-08-08 12:34:10 +0900

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🩷 코인여친 CoinGirlfriend 🩷</h1>
+      <p>여긴 코인 차트 감성 분석 챗봇 공간이야! 곧 다시 만나요~</p>
+    </div>
+  );
+}
